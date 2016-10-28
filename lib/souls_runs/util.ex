2710ad0998900e2e4c96bb9c0cee595f5e13d457
rekyuu <senkan.re.kyuu@gmail.com>
2016-10-28 04:01:20 -0600

    fsdf

--- lib/souls_runs/util.ex
+++ lib/souls_runs/util.ex
@@ -101,7 +101,7 @@ defmodule SoulsRuns.Util do
       armor: "#{Enum.random(defs[:armor])} Set",
       spells: spells,
       quests: Enum.take_random(defs[:quests], 5),
-      challenges: Enum.take_random(SoulsRuns.Util.Defs.challenge, 3)
+      challenges: Enum.take_random(SoulsRuns.Util.Defs.challenges, 3)
     }
   end
 end
